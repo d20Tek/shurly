eb8f79cc8009fa0c74718e2d97d56ef70b7c69b2
Pedro Silva <pedrosi@microsoft.com>
2023-10-04 14:11:26 -0700

    Added OperationManager class to wrap repository code and any other operations that we don't want to leak exceptions.

--- src/D20Tek.Shurly.Infrastructure/ShortenedUrlRepository.cs
+++ src/D20Tek.Shurly.Infrastructure/ShortenedUrlRepository.cs
@@ -1,46 +1,61 @@
 ﻿//---------------------------------------------------------------------------------------------------------------------
 // Copyright (c) d20Tek.  All rights reserved.
 //---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Minimal.Domain;
 using D20Tek.Shurly.Application.Abstractions;
 using D20Tek.Shurly.Domain.ShortenedUrl;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace D20Tek.Shurly.Infrastructure;
 
 internal class ShortenedUrlRepository : IShortenedUrlRepository
 {
     private readonly ShurlyDbContext _dbContext;
+    private readonly OperationManager _opsManager;
 
-    public ShortenedUrlRepository(ShurlyDbContext dbContext)
+    public ShortenedUrlRepository(
+        ShurlyDbContext dbContext,
+        ILogger<ShortenedUrlRepository> logger)
     {
         _dbContext = dbContext;
+        _opsManager = new OperationManager(logger, nameof(ShortenedUrlRepository));
     }
 
     public async Task<IList<ShortenedUrl>> GetForOwnerAsync(AccountId ownerId)
     {
-        var entities = await _dbContext.ShortenedUrls
+        return await _opsManager.OperationAsync<IList<ShortenedUrl>>(async () =>
+        {
+            var entities = await _dbContext.ShortenedUrls
             .Where(x => x.UrlMetadata.OwnerId == ownerId)
             .OrderBy(x => x.UrlMetadata.CreatedOn)
             .ToListAsync();
 
-        return entities;
+            return entities;
+        }) ?? Array.Empty<ShortenedUrl>();
     }
 
     public async Task<ShortenedUrl?> GetByIdAsync(ShortenedUrlId id)
     {
-        var entity = await _dbContext.ShortenedUrls
-            .SingleOrDefaultAsync(x => x.Id == id);
+        return await _opsManager.OperationAsync<ShortenedUrl?>(async () =>
+        {
+            var entity = await _dbContext.ShortenedUrls
+                .SingleOrDefaultAsync(x => x.Id == id);
 
-        return entity;
+            return entity;
+        });
     }
 
     public async Task<ShortenedUrl?> GetByShortUrlCodeAsync(ShortUrlCode code)
     {
-        var entity = await _dbContext.ShortenedUrls
+        return await _opsManager.OperationAsync<ShortenedUrl?>(async () =>
+        {
+            var entity = await _dbContext.ShortenedUrls
             .SingleOrDefaultAsync(
                 x => x.ShortUrlCode == code && x.UrlMetadata.State == UrlState.Published);
 
-        return entity;
+            return entity;
+        });
     }
 
     public async Task<bool> IsUrlCodeUniqueAsync(string shortUrlCode) =>
@@ -48,24 +63,30 @@ internal class ShortenedUrlRepository : IShortenedUrlRepository
 
     public async Task<bool> CreateAync(ShortenedUrl shortenedUrl)
     {
-        await _dbContext.ShortenedUrls.AddAsync(shortenedUrl);
-        await _dbContext.SaveChangesAsync();
-
-        return true;
+        return await _opsManager.ValueOperationAsync<bool>(async () =>
+        {
+            await _dbContext.ShortenedUrls.AddAsync(shortenedUrl);
+            var itemsSaved = await _dbContext.SaveChangesAsync();
+            return itemsSaved > 0;
+        });
     }
 
     public async Task<bool> UpdateAsync(ShortenedUrl shortenedUrl)
     {
-        await _dbContext.SaveChangesAsync();
-
-        return true;
+        return await _opsManager.ValueOperationAsync<bool>(async () =>
+        {
+            await _dbContext.SaveChangesAsync();
+            return true;
+        });
     }
 
     public async Task<bool> DeleteAsync(ShortenedUrl shortenedUrl)
     {
-        _dbContext.ShortenedUrls.Remove(shortenedUrl);
-        await _dbContext.SaveChangesAsync();
-
-        return true;
+        return await _opsManager.ValueOperationAsync<bool>(async () =>
+        {
+            _dbContext.ShortenedUrls.Remove(shortenedUrl);
+            var itemsSaved = await _dbContext.SaveChangesAsync();
+            return itemsSaved > 0;
+        });
     }
 }

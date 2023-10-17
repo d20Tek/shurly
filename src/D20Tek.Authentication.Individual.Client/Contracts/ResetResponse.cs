80dc3b11bf00c5141a26b85369b2577d712f7d26
Pedro Silva <pedrosi@microsoft.com>
2023-10-17 14:23:34 -0700

    Removed Auth.Individual.Client project. Replaced it with reference to Auth.Individual.Client nuget package.

--- src/D20Tek.Authentication.Individual.Client/Contracts/ResetResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿//---------------------------------------------------------------------------------------------------------------------
-// Copyright (c) d20Tek.  All rights reserved.
-//---------------------------------------------------------------------------------------------------------------------
-namespace D20Tek.Authentication.Individual.Client.Contracts;
-
-public sealed record ResetResponse(string ResetToken);

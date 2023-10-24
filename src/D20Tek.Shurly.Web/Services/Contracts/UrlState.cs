cd96c44730da93340f60f5f173e0d92f6da54dcc
Pedro Silva <pedrosi@microsoft.com>
2023-10-23 17:06:04 -0700

    Added ShortUrlList page. Componentized the items with that list. Componentized parts for buttons and labels. Added UrlState to ShortenedUrlResponse.

--- src/D20Tek.Shurly.Web/Services/Contracts/UrlState.cs
+++ src/D20Tek.Shurly.Web/Services/Contracts/UrlState.cs
@@ -3,14 +3,9 @@
 //---------------------------------------------------------------------------------------------------------------------
 namespace D20Tek.Shurly.Web.Services.Contracts;
 
-public sealed record ShortenedUrlResponse(
-    string Id,
-    string Title,
-    string LongUrl,
-    string ShortUrl,
-    string Summary,
-    List<string> Tags,
-    int ClickCount,
-    UrlState State,
-    DateTime CreatedOn,
-    DateTime PublishOn);
+public enum UrlState
+{
+    New,
+    Published,
+    Obsolete
+}

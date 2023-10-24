992706c9ed7fedc90cdc954fa09463a3dc741e82
Pedro Silva <pedrosi@microsoft.com>
2023-10-24 13:38:29 -0700

    Added NavigationManager extension methods to provide GoBack and GoForward methods. Updated Delete page to use it.

---
+++ src/D20Tek.Shurly.Web/NavigationManagerExtensions.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace D20Tek.Shurly.Web;
+
+public static class NavigationManagerExtensions
+{
+    public static async Task GoBack(this NavigationManager nav, IJSRuntime jsRuntime)
+    {
+        await jsRuntime.InvokeVoidAsync("history.back");
+    }
+
+    public static async Task GoForward(this NavigationManager nav, IJSRuntime jsRuntime)
+    {
+        await jsRuntime.InvokeVoidAsync("history.forward");
+    }
+}

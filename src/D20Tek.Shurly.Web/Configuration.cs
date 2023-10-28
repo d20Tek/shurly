f9297dd410f208fe956101961a84891be0552154
Pedro Silva <pedrosi@microsoft.com>
2023-10-28 12:13:52 -0700

    Refactor ShortUrlItem code. Moved PublishStateButton code to code behind file.

---
+++ src/D20Tek.Shurly.Web/Configuration.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Shurly.Web;
+
+internal static class Configuration
+{
+    public static class ShortUrl
+    {
+        public const string ListUrl = $"/short-url";
+
+        public const string CreateUrl = $"/short-url/create";
+
+        public static string EditUrl(string id) => $"/short-url/edit/{id}";
+
+        public static string DeleteUrl(string id) => $"/short-url/delete/{id}";
+
+        public static string ViewUrl(string id) => $"/short-url/view/{id}";
+    }
+}
---
+++ src/D20Tek.Shurly.Web/Pages/ShortUrl/PublishStateButton.razor.cs
@@ -0,0 +1,38 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Shurly.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace D20Tek.Shurly.Web.Pages.ShortUrl;
+
+public partial class PublishStateButton
+{
+    private UrlState LinkState => Link?.State ?? UrlState.New;
+
+    [Parameter]
+    public ShortenedUrlResponse? Link { get; set; }
+
+    [Parameter]
+    public EventCallback<ShortenedUrlResponse> PublishStateChanged { get; set; }
+
+    private async Task OnPublish()
+    {
+        if (Link is null) return;
+
+        var result = await _service.PublishAsync(Link.Id);
+        result.MatchFirstError(
+            success => PublishStateChanged.InvokeAsync(success),
+            error => Console.WriteLine($"Publish Error: {error}"));
+    }
+
+    private async Task OnUnpublish()
+    {
+        if (Link is null) return;
+
+        var result = await _service.UnpublishAsync(Link.Id);
+        result.MatchFirstError(
+            success => PublishStateChanged.InvokeAsync(success),
+            error => Console.WriteLine($"Unpublish Error: {error}"));
+    }
+}
